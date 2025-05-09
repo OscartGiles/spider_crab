364204944f10b914abb348d203938d4edad771cc
OscartGiles <oscart.giles@gmail.com>
2025-05-09 15:35:07 +0100

    Fix bench marking

--- benches/html_parser.rs
+++ benches/html_parser.rs
@@ -1,19 +1,21 @@
 use spider_crab::{parse_links, PageContent};
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use std::hint::black_box;
+use std::{hint::black_box, sync::Arc};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let html = std::fs::read_to_string("./tests/test_data/monzo/home.html").unwrap();
 
-    let page = PageContent {
+    let page = Arc::new(PageContent {
         url: "https://monzo.com".parse().unwrap(),
         status_code: reqwest::StatusCode::OK,
         content: html.clone(),
         content_type: Some("text/html".parse().unwrap()),
-    };
+    });
 
-    c.bench_function("parse html", |b| b.iter(|| parse_links(black_box(&page))));
+    c.bench_function("parse html", |b| {
+        b.iter(|| parse_links(black_box(page.clone())))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
